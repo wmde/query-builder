ac9f27f12f01db2671291cbec2dab0b1808f9fe2
Amir Sarabadani <Ladsgroup@gmail.com>
2020-10-06 11:01:49 +0200

    Fixes

--- build/updater.sh
+++ build/updater.sh
@@ -2,8 +2,8 @@
 cd /data/project/query-builder-test/query-builder
 git reset --hard origin/master
 git pull
-./node_modules/.bin/npm install npm@6.14.8
-./node_modules/.bin/npm update
+npm install npm@6.14.8
+./node_modules/.bin/npm install
 ./node_modules/.bin/npm run build
 rm -rf public_html/*
 cp -R dist/* ../public_html/
