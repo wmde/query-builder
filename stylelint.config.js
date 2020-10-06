930f70536c9ee3da672ee583873fce7d19be2d50
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-10-06 16:51:45 +0200

    Enforce CSS logical properties via stylelint
    Same as
    https://github.com/wmde/wikit/commit/bbbac8e490a838a862c7bcf5ffb7b2b546e8f841
    and
    https://github.com/wmde/wikit/commit/31007423587a285fecbec3538cd88c1ebd3ffa30

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	extends: [
+		'stylelint-config-standard'
+	],
+	plugins: [
+		'stylelint-use-logical'
+	],
+	rules: {
+		indentation: 'tab',
+		'csstools/use-logical': 'always',
+		// CSS Logical Properties do not support the shorthand 'margin' and 'padding' yet
+		'property-disallowed-list': [ '/^margin$/', '/^padding$/' ]
+	}
+};
