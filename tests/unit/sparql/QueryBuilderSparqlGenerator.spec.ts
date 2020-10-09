5cc90056c6e7800e716886fe77b12deeaa3bd7b7
Amir Sarabadani <Ladsgroup@gmail.com>
2020-10-09 11:41:49 +0200

    Copy the Sparqljs type to here

--- tests/unit/sparql/QueryBuilderSparqlGenerator.spec.ts
+++ tests/unit/sparql/QueryBuilderSparqlGenerator.spec.ts
@@ -1,6 +1,6 @@
 import QueryBuilderSparqlGenerator from '@/sparql/QueryBuilderSparqlGenerator';
 import SparqlGenerator from '@/sparql/SparqlGenerator';
-import QueryObject from '@/sparql/QueryObject';
+import Sparqljs from '@/sparql/Sparqljs';
 
 describe( 'QueryBuilderSparqlGenerator', () => {
 
@@ -38,7 +38,7 @@ describe( 'QueryBuilderSparqlGenerator', () => {
 		const prefixes = {
 			wdt: 'http://www.wikidata.org/prop/direct/'
 		};
-		const queryObject: QueryObject = {
+		const queryObject: Sparqljs.SelectQuery = {
 			queryType: 'SELECT',
 			variables: [
 				{
