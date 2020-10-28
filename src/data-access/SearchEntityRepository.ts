84204a87edfd14cbb1cf41d2e053224c1f17f730
Michael Große <mic.grosse@googlemail.com>
2020-10-28 19:28:46 +0100

    Add SearchEntityRepository interface
    Pagination will be necessary to implement the "load more on scroll"
    feature: https://phabricator.wikimedia.org/T266856
    
    The actual server response contains a lot more information than just
    what is currently defined in SearchResult. But it seems sensible to keep
    the interface limited to what we really need just for now.
    
    Bug: T266555

---
+++ src/data-access/SearchEntityRepository.ts
@@ -0,0 +1,9 @@
+import SearchResult from './SearchResult';
+
+/**
+ * Repository to search for entities.
+ * The language will be defined in the constructor as will be further options
+ */
+export default interface SearchEntityRepository {
+    searchProperties( searchString: string, limit?: number, offset?: number ): Promise<Array<SearchResult>>;
+}
