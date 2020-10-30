6e03477baebe68103548c4c86e1aeb408af31bc1
Michael Große <mic.grosse@googlemail.com>
2020-10-30 17:27:38 +0100

    Add implementation for repository to search for entities
    Bug: T266549

---
+++ src/data-access/FetchSearchEntityRepository.ts
@@ -0,0 +1,52 @@
+import SearchEntityRepository from '@/data-access/SearchEntityRepository';
+import SearchResult from '@/data-access/SearchResult';
+import TechnicalProblem from '@/data-access/errors/TechnicalProblem';
+
+export default class FetchSearchEntityRepository implements SearchEntityRepository {
+	private readonly forLanguageCode: string;
+	private readonly endpoint: string;
+
+	public constructor( languageCode: string, endpoint: string ) {
+		this.forLanguageCode = languageCode;
+		this.endpoint = endpoint;
+	}
+
+	public async searchProperties( searchString: string, limit?: number, offset?: number ):
+	Promise<SearchResult[]> {
+		const params: { [key: string]: string } = {
+			action: 'wbsearchentities',
+			search: searchString,
+			language: this.forLanguageCode,
+			type: 'property',
+			format: 'json',
+			formatversion: '2',
+			errorformat: 'plaintext',
+			origin: '*'
+		};
+		if ( limit ) {
+			params.limit = `${limit}`;
+		}
+		if ( offset ) {
+			params.offset = `${offset}`;
+		}
+
+		const url = new URL(this.endpoint);
+		for ( const key in params ) {
+			url.searchParams.set( key, params[key] );
+		}
+		let response: Response;
+		try {
+			response = await fetch( url.toString() );
+		} catch ( e ) {
+			throw new TechnicalProblem( 'Network error' );
+		}
+
+		if ( !response.ok ) {
+			throw new TechnicalProblem( `${response.status}: ${response.statusText}` );
+		}
+
+		const data = await response.json();
+
+		return data.search;
+	}
+}
---
+++ tests/unit/data-access/FetchSearchEntityRepository.spec.ts
@@ -0,0 +1,80 @@
+import FetchSearchEntityRepository from '@/data-access/FetchSearchEntityRepository';
+import TechnicalProblem from '@/data-access/errors/TechnicalProblem';
+
+describe( 'FetchSearchEntityRepository', () => {
+
+	it( 'searches for properties with default values', async () => {
+		const testLang = 'eo';
+		const testEndpoint = 'https://example.com/w/api.php';
+		const repo = new FetchSearchEntityRepository(
+			testLang,
+			testEndpoint
+		);
+		const testSearchTerm = '"><script>alert(\'XXS!\');</script>';
+		const expectedResult = [ { foo: 'bar' } ];
+
+		window.fetch = jest.fn().mockImplementation(() => Promise.resolve( {
+			ok: true,
+				json: async () => ( { search: expectedResult } ),
+		} ) );
+
+		const actualResult = await repo.searchProperties( testSearchTerm );
+
+		const escapedSearch = '%22%3E%3Cscript%3Ealert%28%27XXS%21%27%29%3B%3C%2Fscript%3E';
+		const expectedUrl = `${testEndpoint}?action=wbsearchentities&search=${escapedSearch}&language=${testLang}&type=property&format=json&formatversion=2&errorformat=plaintext&origin=*`;
+		expect( window.fetch ).toHaveBeenCalledTimes(1);
+		expect( window.fetch ).toHaveBeenCalledWith( expectedUrl );
+		expect( actualResult ).toBe( expectedResult );
+	} );
+
+	it( 'searches for properties with provided limit and offset', async() => {
+		const testLang = 'eo';
+		const testEndpoint = 'https://example.com/w/api.php';
+		const repo = new FetchSearchEntityRepository(
+			testLang,
+			testEndpoint
+		);
+		const testSearch = 'instance';
+		window.fetch = jest.fn().mockImplementation(() => Promise.resolve( {
+			ok: true,
+			json: async () => ( { search:  [ { foo: 'bar' } ] } ),
+		} ) );
+		const limit = 12;
+		const offset = 24;
+
+		await repo.searchProperties( testSearch, limit, offset );
+
+		const expectedUrl = `${testEndpoint}?action=wbsearchentities&search=${testSearch}&language=${testLang}&type=property&format=json&formatversion=2&errorformat=plaintext&origin=*&limit=${limit}&offset=${offset}`;
+		expect( window.fetch ).toHaveBeenCalledTimes(1);
+		expect( window.fetch ).toHaveBeenCalledWith( expectedUrl );
+	} );
+
+	it( 'throws an error if there is a server side problem', () => {
+		const repo = new FetchSearchEntityRepository(
+			'eo',
+			'https://example.com/w/api.php'
+		);
+		window.fetch = jest.fn().mockImplementation(() => Promise.resolve( {
+			ok: false,
+			status: 500,
+			statusText: 'Server Error',
+		} ) );
+
+		const expectedError = new TechnicalProblem( '500: Server Error');
+
+		expect( repo.searchProperties( 'instance' ) ).rejects.toThrow( expectedError );
+	} );
+
+	it( 'throws an error if there is a network problem', () => {
+		const repo = new FetchSearchEntityRepository(
+			'eo',
+			'https://example.com/w/api.php'
+		);
+		window.fetch = jest.fn().mockImplementation(() => Promise.reject() );
+
+		const expectedError = new TechnicalProblem( 'Network error');
+
+		expect( repo.searchProperties( 'instance' ) ).rejects.toThrow( expectedError );
+	} );
+
+} );
