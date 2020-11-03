67aa73a053e01cbf234a6d1c3829d249cf2a8658
Amir Sarabadani <Ladsgroup@gmail.com>
2020-11-03 20:11:30 +0100

    Add styles for links (#41)
    * Add styles for links
    
    We added links to descriptions and now we need to add styles for that.
    
    Based on https://www.figma.com/file/gi3JYwSUQYhKS0mHwK25cQwi/WiKit?node-id=2510%3A3550
    and tokens
    
    * Update src/components/QueryBuilder.vue
    
    Co-authored-by: Sai_San <sarai.sanchez@wikimedia.de>
    
    * Update QueryBuilder.vue
    
    * Update QueryBuilder.vue
    
    Co-authored-by: Sai_San <sarai.sanchez@wikimedia.de>

--- src/components/QueryBuilder.vue
+++ src/components/QueryBuilder.vue
@@ -75,6 +75,20 @@ export default Vue.extend( {
 </script>
 
 <style scoped lang="scss">
+// TODO replace with link component once available
+a {
+	font-family: $font-family-style-link;
+	font-style: $font-size-style-link;
+	font-weight: $font-weight-style-link;
+	font-size: $font-size-style-link;
+	line-height: $font-line-height-style-link;
+	color: $font-color-link-default;
+
+	&:visited {
+		color: $font-color-link-visited;
+	}
+}
+
 .querybuilder {
 	padding-block: $dimension-spacing-xlarge;
 	padding-inline: $dimension-spacing-xlarge;
