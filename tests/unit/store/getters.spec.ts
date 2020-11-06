fcb1d80229c9d0c80cf8c81e8ab19be906897418
Michael Große <mic.grosse@googlemail.com>
2020-11-06 20:32:32 +0100

    Add test for the nontrivial query getter

---
+++ tests/unit/store/getters.spec.ts
@@ -0,0 +1,25 @@
+import RootState from '@/store/RootState';
+import getters from '@/store/getters';
+import QueryRepresentation from '@/sparql/QueryRepresentation';
+
+describe( 'getters', () => {
+	describe( 'query', () => {
+		it( 'returns the QueryRepresentation of the RootState', () => {
+			const state: RootState = {
+				conditionRow: {
+					valueData: { value: 'foo' },
+					propertyData: { id: 'P123', label: 'abc' },
+				},
+			};
+
+			const expectedValue: QueryRepresentation = {
+				condition: {
+					propertyId: 'P123',
+					value: 'foo',
+				},
+			};
+
+			expect( getters.query( state ) ).toStrictEqual( expectedValue );
+		} );
+	} );
+} );
