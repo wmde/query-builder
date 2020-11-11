131b08620521fafa67f14ac589d684af3bcfa41c
Amir Sarabadani <Ladsgroup@gmail.com>
2020-11-11 15:01:20 +0100

    Fix validator of property values (#57)
    * Fix validator of property values
    
    Currently, if the property is empty, the property returns an empty
    object that has "id" property and we can check against that instead.
    
    Without this patch, it thinks the property is filled all the time.
    
    Also, renaming the property to its correct name.
    
    * Move the check to getter level

--- src/components/QueryBuilder.vue
+++ src/components/QueryBuilder.vue
@@ -10,7 +10,7 @@
 			<h2 class="querybuilder__find-title">Find all items...</h2>
 			<div class="querybuilder__rule">
 				<PropertyLookup
-					v-model="selectedItem"
+					v-model="selectedProperty"
 					:error="fieldErrors.property"
 				/>
 				<TextInput
@@ -60,7 +60,7 @@ export default Vue.extend( {
 		validate(): boolean {
 			this.errors = [];
 			this.$store.dispatch( 'setErrors', [] );
-			if ( !this.selectedItem && !this.textInputValue ) {
+			if ( !this.selectedProperty && !this.textInputValue ) {
 				this.errors.push( {
 					// eslint-disable-next-line max-len
 					message: 'Looks like the Query Builder was empty, please enter a valid query first, then try running it again',
@@ -69,8 +69,8 @@ export default Vue.extend( {
 				return false;
 			}
 
-			if ( !this.selectedItem || !this.textInputValue ) {
-				if ( !this.selectedItem ) {
+			if ( !this.selectedProperty || !this.textInputValue ) {
+				if ( !this.selectedProperty ) {
 					this.fieldErrors.property = {
 						message: 'Please select a property',
 						type: 'error',
@@ -102,10 +102,12 @@ export default Vue.extend( {
 		},
 	},
 	computed: {
-		selectedItem: {
-			get(): Property { return this.$store.getters.property; },
-			set( selectedItem: SearchResult ): void {
-				this.$store.dispatch( 'updateProperty', selectedItem );
+		selectedProperty: {
+			get(): Property | null {
+				return this.$store.getters.property;
+			},
+			set( selectedProperty: SearchResult ): void {
+				this.$store.dispatch( 'updateProperty', selectedProperty );
 			},
 		},
 		textInputValue: {
