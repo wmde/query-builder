f41d05db35cc4391adf52b86e16ff5fc09586749
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-11-20 15:41:13 +0100

    Adjust Query Representation and SPARQL Query for without state
    + added without state to enum
    + modified QueryRepresentation by adding without state
    + added test to verify that query is being generated correctly

--- tests/unit/sparql/buildQuery.spec.ts
+++ tests/unit/sparql/buildQuery.spec.ts
@@ -14,6 +14,26 @@ describe( 'buildQuery', () => {
 		} } ) ).toEqual( `SELECT ?item WHERE { ?item (p:${propertyId}/ps:${propertyId}) "${value}". }` );
 	} );
 
+	it( 'builds a query from a property and a string value with not matching selected', () => {
+		const propertyId = 'P666';
+		const value = 'blah';
+		const propertyValueRelation = PropertyValueRelation.NotMatching;
+
+		const expectedQuery =
+			`SELECT ?item WHERE {
+			?item (p:${propertyId}/ps:${propertyId}) ?instance.
+			FILTER(?instance != "${value}")
+			}`;
+
+		const receivedQuery = buildQuery( { condition: {
+			propertyId,
+			value,
+			propertyValueRelation,
+		} } );
+
+		expect( receivedQuery.replace( /\s+/g, ' ' ) ).toEqual( expectedQuery.replace( /\s+/g, ' ' ) );
+	} );
+
 	it( 'builds a query from a property with "any value" selected', () => {
 		const propertyId = 'P666';
 		const propertyValueRelation = PropertyValueRelation.Regardless;
