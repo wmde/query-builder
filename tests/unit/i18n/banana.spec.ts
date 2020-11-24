f31160fd5913025c25b032c249b9f4ba1712c7a5
Amir Sarabadani <Ladsgroup@gmail.com>
2020-11-24 16:53:18 +0100

    Add banana tests (#73)
    - Make sure all messages start with "query-builder-" prefix to avoid
       clashes in translatewiki.
     - Make sure all en.json messages have description in qqq.json and we
       don't have any orphan descriptions either.
    
    Bug: T263553

---
+++ tests/unit/i18n/banana.spec.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+
+describe( 'banana tests', () => {
+	it( 'messages keys start with query-builder', () => {
+		const data = fs.readFileSync( 'public/i18n/en.json', 'utf8' );
+		const messages: { [key: string]: { [key: string]: string} } = {
+			en: JSON.parse( data ),
+		};
+		let messageKey = '';
+
+		for ( messageKey in messages.en ) {
+			expect( messageKey.startsWith( 'query-builder-' ) ).toBeTruthy();
+		}
+	} );
+
+	it( 'messages keys of en.json and qqq.json should be the same', () => {
+		const dataEn = fs.readFileSync( 'public/i18n/en.json', 'utf8' );
+		const dataQqq = fs.readFileSync( 'public/i18n/qqq.json', 'utf8' );
+		const messages: { [key: string]: { [key: string]: string} } = {
+			en: JSON.parse( dataEn ),
+			qqq: JSON.parse( dataQqq ),
+		};
+		let messageKey = '';
+
+		for ( messageKey in messages.en ) {
+			expect( messages.qqq[ messageKey ] ).toBeTruthy();
+		}
+
+		for ( messageKey in messages.qqq ) {
+			expect( messages.en[ messageKey ] ).toBeTruthy();
+		}
+	} );
+} );
