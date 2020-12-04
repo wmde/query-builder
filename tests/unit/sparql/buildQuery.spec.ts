4a67a0da2aae4ca76fa9717d6d9030280ad18671
Amir Sarabadani <Ladsgroup@gmail.com>
2020-12-04 12:50:10 +0100

    Fixes

--- tests/unit/sparql/buildQuery.spec.ts
+++ tests/unit/sparql/buildQuery.spec.ts
@@ -51,7 +51,7 @@ describe( 'buildQuery', () => {
 		] } ) ).toEqual( `SELECT ?item WHERE { ?item (p:${propertyId}/ps:${propertyId}) _:anyValue. }` );
 	} );
 
-	it( 'builds a query from multiple conditions', () => {
+	it( 'builds a query from multiple conditions, one matching and one regardless', () => {
 		const expectedQuery =
 			`SELECT ?item WHERE {
 			?item (p:P666/ps:P666) "blah".
@@ -67,6 +67,52 @@ describe( 'buildQuery', () => {
 				propertyId: 'P66',
 				value: '',
 				propertyValueRelation: PropertyValueRelation.Regardless,
+			},
+		] } );
+
+		expect( actualQuery.replace( /\s+/g, ' ' ) ).toEqual( expectedQuery.replace( /\s+/g, ' ' ) );
+	} );
+
+	it( 'builds a query from multiple conditions, one matching and one non-matching', () => {
+		const expectedQuery =
+			`SELECT ?item WHERE {
+			?item (p:P666/ps:P666) "blah".
+			?item (p:P66/ps:P66) ?instance.
+			FILTER(?instance != "foo")
+			}`;
+		const actualQuery = buildQuery( { conditions: [
+			{
+				propertyId: 'P666',
+				value: 'blah',
+				propertyValueRelation: PropertyValueRelation.Matching,
+			},
+			{
+				propertyId: 'P66',
+				value: 'foo',
+				propertyValueRelation: PropertyValueRelation.NotMatching,
+			},
+		] } );
+
+		expect( actualQuery.replace( /\s+/g, ' ' ) ).toEqual( expectedQuery.replace( /\s+/g, ' ' ) );
+	} );
+
+	it( 'builds a query from multiple conditions, one non-matching and one matching', () => {
+		const expectedQuery =
+			`SELECT ?item WHERE {
+			?item (p:P666/ps:P666) ?instance.
+			FILTER(?instance != "blah")
+			?item (p:P66/ps:P66) "foo".
+			}`;
+		const actualQuery = buildQuery( { conditions: [
+			{
+				propertyId: 'P666',
+				value: 'blah',
+				propertyValueRelation: PropertyValueRelation.NotMatching,
+			},
+			{
+				propertyId: 'P66',
+				value: 'foo',
+				propertyValueRelation: PropertyValueRelation.Matching,
 			},
 		] } );
 
