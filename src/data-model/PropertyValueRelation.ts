5de87fdbf1791486b3cf55e93ac907be6a7f6dc8
Ichi Rarámuri <guergana.tzatchkova@wikimedia.de>
2020-12-14 16:28:54 +0100

    Translate value type relation (#97)
    * added i18n translation description and english translation
    
    * made ValueTypeDropdown use i18n
    updated ValueTypeDropdown test to use i18n

--- src/data-model/PropertyValueRelation.ts
+++ src/data-model/PropertyValueRelation.ts
@@ -1,7 +1,9 @@
 enum PropertyValueRelation {
+	// these values also used as part of message keys and
+	// so any changes made to this enum have to also be reflected in en.json and qqq.json
 	Matching = 'matching',
 	NotMatching = 'without',
-	Regardless = 'regardless of value',
+	Regardless = 'regardless-of-value',
 }
 
 export default PropertyValueRelation;
