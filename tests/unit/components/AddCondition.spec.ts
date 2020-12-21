a4458895950a3d86a184fec6004878e08101bec9
Ichi Rarámuri <guergana.tzatchkova@wikimedia.de>
2020-12-21 13:04:34 +0100

    Fixed warning in addcontion unit test (#113)
    This warning was showing in the tests because the i18n library
    was not imported in the tests
    [Vue warn]: Failed to resolve directive: i18n (found in <AddCondition>)

--- tests/unit/components/AddCondition.spec.ts
+++ tests/unit/components/AddCondition.spec.ts
@@ -2,6 +2,14 @@ import Vue from 'vue';
 import AddCondition from '@/components/AddCondition.vue';
 import { Button } from '@wmde/wikit-vue-components';
 import { shallowMount } from '@vue/test-utils';
+import i18n from 'vue-banana-i18n';
+
+const messages = {};
+Vue.use( i18n, {
+	locale: 'en',
+	messages,
+	wikilinks: true,
+} );
 
 describe( 'AddCondition.vue', () => {
 	it( 'emits an `add-condition` event when Add Condition button is clicked', async () => {
