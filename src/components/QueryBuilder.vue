50f4151fd95b639e79537da72a8b2549fb8120a9
Michael Große <mic.grosse@googlemail.com>
2021-01-07 18:29:32 +0100

    Fix direction detection
    Detecting the direction did not work previously. We need the <bdi> to
    make the browser actually compute the direction before we can read it
    with getComputedStyle.

--- src/components/QueryBuilder.vue
+++ src/components/QueryBuilder.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="querybuilder" role="main">
-		<h1 class="querybuilder__heading"
-			v-i18n="{msg: 'query-builder-heading'}" />
+		<h1 class="querybuilder__heading"><bdi id="directionSample"
+			v-i18n="{msg: 'query-builder-heading'}" /></h1>
 		<p class="querybuilder__description">
 			Welcome to the test system of the Simple Query Builder. Please note that this is a work in progress,
 			not all features are included, and it can sometimes be broken.
