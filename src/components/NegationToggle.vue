0305fd98cdab7b79e5650a9b2edd63fcacdf105d
Michael Große <mic.grosse@googlemail.com>
2021-01-14 13:04:25 +0100

    Add NegationToggle component
    Bug: T272011

---
+++ src/components/NegationToggle.vue
@@ -0,0 +1,36 @@
+<template>
+	<span class="negationToggle">
+		<Button
+			:type="value==='with' ? 'progressive' : 'neutral'"
+			:variant="value==='with' ? 'primary' : 'normal'"
+			@click.native="$emit('input', 'with')"
+		>{{ $i18n( 'query-builder-negation-option-label-with' ) }}</Button>
+		<Button
+			:type="value==='without' ? 'progressive' : 'neutral'"
+			:variant="value==='without' ? 'primary' : 'normal'"
+			@click.native="$emit('input', 'without')"
+		>{{ $i18n( 'query-builder-negation-option-label-without' ) }}</Button>
+	</span>
+</template>
+
+<script lang="ts">
+import { Button } from '@wmde/wikit-vue-components';
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'NegationToggle',
+	components: { Button },
+	props: {
+		value: {
+			required: true,
+			type: String,
+		},
+	},
+} );
+</script>
+
+<style scoped lang="scss">
+	.negationToggle {
+		white-space: nowrap;
+	}
+</style>
