a414cb72c5e7affb0872c0a91436e307b8eec920
Michael Große <michael.grosse@wikimedia.de>
2021-01-15 16:39:50 +0100

    Add NegationToggle component (#152)
    The tests will be added when we start using the ButtonToggle component from the Design System.
    
    Bug: T272011

---
+++ tests/unit/components/NegationToggle.spec.ts
@@ -0,0 +1,6 @@
+describe( 'NegationToggle', () => {
+	// TODO: Write those tests when using the upcoming ButtonToggle component -> T271788
+	it.todo( 'passes the value down to the ButtonToggle' );
+	it.todo( 'passes down options for negation to the Button Toggle' );
+	it.todo( 'bubbles up the input event from the ButtonToggle' );
+} );
