5e6a917169fc8508f39e44b5978d0628ebfa2684
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-01-15 15:59:51 +0100

    Create SubclassCheckbox component

---
+++ src/components/SubclassCheckbox.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="querybuilder__include-subclasses">
+		<input
+			class="querybuilder__include-subclasses-checkbox"
+			type="checkbox"
+			id="include-subclasses"
+			:checked="isChecked"
+			@change="$emit( 'subclass-check', $event )"
+		/>
+		<label for="include-subclasses">
+			<span
+				v-i18n="{msg: 'query-builder-include-subclasses'}" />
+		</label>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend( {
+	name: 'SubclassCheckbox',
+	props: {
+		isChecked: {
+			type: Boolean,
+			required: true,
+		},
+	},
+} );
+</script>
+
+<style lang="scss">
+.querybuilder__include-subclasses {
+	display: flex;
+	align-items: flex-end;
+	color: $font-color-base;
+	// TODO: change to real ones
+	font-family: $font-family-style-description;
+	margin-block-start: $dimension-layout-medium;
+
+	&-checkbox {
+		// TODO: change to real ones
+		margin-inline-end: $dimension-layout-xxsmall;
+	}
+}
+</style>
