7c5a73f08c6efbffbbc6298dea0ad54f9188fd0e
Michael Große <mic.grosse@googlemail.com>
2021-01-22 17:24:59 +0100

    Don't fail eslint on node 10 features

--- tests/e2e/fake/functions/hello.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async ( event ) => {
-	const cookieHeader = event.headers.cookie;
-	const subject = event.queryStringParameters.name || 'World';
-	return {
-		statusCode: 200,
-		body: `Hello ${subject}! ${cookieHeader} ${JSON.stringify( cookieHeader )}`,
-	};
-};
