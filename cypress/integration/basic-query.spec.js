c241ce730103170d924e7048c06200f26a434b80
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-01-28 17:29:17 +0100

    Wait for search requests instead of hardcoding time

--- cypress/integration/basic-query.spec.js
+++ cypress/integration/basic-query.spec.js
@@ -1,23 +1,36 @@
 const { createYield } = require( 'typescript' );
 
+function wikibaseApiRequest( query ) {
+	return {
+		url: 'https://www.wikidata.org/w/api.php',
+		query,
+	};
+}
+
 describe( 'Basic Query', () => {
 	it( 'can create a query with a single condition', () => {
 		cy.visit( '/' );
 
 		cy.get( 'h1' ).should( 'contain', 'Query Builder' );
 
+		cy.intercept(
+			wikibaseApiRequest( { action: 'wbsearchentities', search: 'has pet' } )
+		).as( 'hasPetRequest' );
+
 		// Gets the first input match
 		cy.get( '.query-condition__property-lookup .wikit-Input' )
-			.type( 'has pet' );
-
-		cy.wait( 4000 );
+			.type( 'has pet' ).wait( '@hasPetRequest' );
 
 		cy.get( '.query-condition__property-lookup .wikit-OptionsMenu__item' ).click();
+
+		cy.intercept(
+			wikibaseApiRequest( { action: 'wbsearchentities', search: 'house cat' } )
+		).as( 'houseCatRequest' );
 
 		cy.get( '.query-condition__value-input .wikit-Input' )
 			.type( 'house cat' );
 
-		cy.wait( 4000 );
+		cy.wait( '@houseCatRequest' );
 
 		cy.get( '.query-condition__value-input .wikit-OptionsMenu__item' )
 			.first()
