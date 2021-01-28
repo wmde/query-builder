4692fb9223a03af6e238f07c371d1fba6c709b67
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-01-28 17:14:49 +0100

    Fix indentation

--- cypress/integration/basic-query.spec.js
+++ cypress/integration/basic-query.spec.js
@@ -1,30 +1,30 @@
-const { createYield } = require("typescript");
+const { createYield } = require( 'typescript' );
 
-describe('Basic Query', () => {
-    it('can create a query with a single condition', () => {
-        cy.visit( '/' );
+describe( 'Basic Query', () => {
+	it( 'can create a query with a single condition', () => {
+		cy.visit( '/' );
 
-        cy.get( 'h1' ).should( 'contain', 'Query Builder' );
+		cy.get( 'h1' ).should( 'contain', 'Query Builder' );
 
-        // Gets the first input match
-       cy.get( '.query-condition__property-lookup .wikit-Input' )
-            .type( 'has pet' );
+		// Gets the first input match
+		cy.get( '.query-condition__property-lookup .wikit-Input' )
+			.type( 'has pet' );
 
-        cy.wait( 4000 );
+		cy.wait( 4000 );
 
-        cy.get( '.query-condition__property-lookup .wikit-OptionsMenu__item' ).click();
+		cy.get( '.query-condition__property-lookup .wikit-OptionsMenu__item' ).click();
 
-        cy.get( '.query-condition__value-input .wikit-Input' )
-          .type( 'house cat' );
+		cy.get( '.query-condition__value-input .wikit-Input' )
+			.type( 'house cat' );
 
-        cy.wait( 4000 );
+		cy.wait( 4000 );
 
-        cy.get( '.query-condition__value-input .wikit-OptionsMenu__item' )
-          .first()
-          .click();
+		cy.get( '.query-condition__value-input .wikit-OptionsMenu__item' )
+			.first()
+			.click();
 
-        cy.get( '.querybuilder__run .wikit-Button' ).click();
+		cy.get( '.querybuilder__run .wikit-Button' ).click();
 
-        cy.get( '.querybuilder__result__iframe').should('be.visible' );
-    });
-})
+		cy.get( '.querybuilder__result__iframe' ).should( 'be.visible' );
+	} );
+} )
