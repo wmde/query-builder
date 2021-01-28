f32d8645a3552fe4c1fcc8676de03150577fc3c1
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-01-28 17:13:50 +0100

    Make the cypress base url dynamic

--- cypress/integration/basic-query.spec.js
+++ cypress/integration/basic-query.spec.js
@@ -2,23 +2,23 @@ const { createYield } = require("typescript");
 
 describe('Basic Query', () => {
     it('can create a query with a single condition', () => {
-        cy.visit('localhost:8080');
+        cy.visit( '/' );
 
         cy.get( 'h1' ).should( 'contain', 'Query Builder' );
-        
+
         // Gets the first input match
        cy.get( '.query-condition__property-lookup .wikit-Input' )
             .type( 'has pet' );
-        
+
         cy.wait( 4000 );
 
         cy.get( '.query-condition__property-lookup .wikit-OptionsMenu__item' ).click();
 
         cy.get( '.query-condition__value-input .wikit-Input' )
           .type( 'house cat' );
-        
+
         cy.wait( 4000 );
-        
+
         cy.get( '.query-condition__value-input .wikit-OptionsMenu__item' )
           .first()
           .click();
