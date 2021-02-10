f54515f36e3074278399a987215515825488b59a
Bereket teshome <bereket.teshome@wikimedia.de>
2021-02-10 17:06:16 +0100

    Rewrite the existing SauceLabs/Nightwatch Browser Test to Cypress and drop SauceLabs
    Bug: T273775

--- tests/e2e/specs/QueryBuilder.test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	'QueryBuilder Component is rendered': ( client ) => {
-		client
-			.init()
-			.waitForElementPresent( 'body' )
-			.assert.visible( '.querybuilder' )
-			.assert.containsText( '.querybuilder__heading', 'Simple Query Builder' );
-	},
-};
