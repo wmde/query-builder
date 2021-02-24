6fbc2886af8ea9d6c455cb6339739ec2e3306c4b
Amir Sarabadani <Ladsgroup@gmail.com>
2021-02-24 19:14:26 +0100

    Install npm on dev mode instead of production mode (#210)
    This avoids errors like sh: 1: vue-cli-service: not found in build

--- build/updater.sh
+++ build/updater.sh
@@ -2,9 +2,9 @@
 cd /data/project/query-builder-test/query-builder
 git reset --hard origin/master
 git pull
-export NODE_ENV=production
 npm install npm@6.14.8
 ./node_modules/.bin/npm install
+export NODE_ENV=production
 ./node_modules/.bin/npm run build
 rm -rf public_html/*
 cp -R dist/* ../public_html/
